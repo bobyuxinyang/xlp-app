4faf5fb570a7ace47af0e74839f3444f20e23d32
YANG Yuxin <bob.yuxinyang@gmail.com>
2018-10-30 15:14:45 +0800

    wip

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const electron = require('electron')
-
+const dockerode = require('dockerode')
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
@@ -8,9 +7,30 @@ function createWindow () {
 
     // Open the DevTools.
     win.webContents.openDevTools()
-    
+
 	// and load the index.html of the app.
 	win.loadFile('index.html')
 }
 
 app.on('ready', createWindow)
+
+const isWindows = () => {
+  return process.platform === 'win32';
+}
+
+let client = null
+
+const setupDocker = () => {
+	try {
+  	if (isWindows()) {
+  	  client = new dockerode({socketPath: '//./pipe/docker_engine'});
+  	} else {
+  	  client = new dockerode({socketPath: '/var/run/docker.sock'});
+  	}
+	} catch (error) {
+		throw new Error('Cannot connect to the Docker daemon. Is the daemon running?');
+	}	
+}
+
+setupDocker()
+console.log('hello: ', client)
